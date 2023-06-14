1fe3c40bf50afe3a23bdf9f47796915fbb6761a3
Simon <46846093+6im0n@users.noreply.github.com>
2023-06-15 01:32:09 +0200

    Create create_files.sh

---
+++ create_files.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+input_file="input.txt"
+
+# Check if the input file exists
+if [ ! -f "$input_file" ]; then
+  echo "Input file '$input_file' not found."
+  exit 1
+fi
+
+# Read the input file line by line
+while IFS= read -r line; do
+  # Extract the last string after the last slash
+  filename="${line##*/}"
+  
+  # Generate the output file name with the extracted filename and .strm extension
+  output_file="${filename}.strm"
+
+  # Write the line to the output file
+  echo "$line" > "$output_file"
+
+  echo "Created file: $output_file"
+done < "$input_file"
